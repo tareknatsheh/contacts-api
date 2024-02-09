7956a8e20ac70268af1cf6ab0ba70c7f4cc1211d
tareknatsheh <tarek.o.natsheh@gmail.com>
2024-02-09 16:48:21 +0200

    clean server.js and fixed connecting to db multiple times by functions in contacts controller

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,6 +1,17 @@
 const { makeContact } = require("../entities/contact");
 const { makeDb } = require("../entities/db");
 
+let db;
+const connectToDatabase = async () => {
+    try {
+        db = await makeDb({ dbName: "mm_api_demo", collectionName: "contacts" });
+    } catch (error) {
+        console.error('Failed to connect to database:', error);
+        process.exit(1); // Exit the application
+    }
+};
+connectToDatabase();
+
 module.exports.handleAddNewContact = async (req, res) => {
     console.log("handling adding new contact controller.");
     let newContact;
@@ -19,7 +30,6 @@ module.exports.handleAddNewContact = async (req, res) => {
         }
 
         // Call the database entity to add a new user
-        const db = await makeDb({ dbName: "mm_api_demo", collectionName: "contacts" });
         const addResult = await db.addOneContact(contactObjToAdd);
 
         return res.status(201).send(addResult);
@@ -33,7 +43,6 @@ module.exports.handleAddNewContact = async (req, res) => {
 
 
 module.exports.handleGetAllContacts = async (req, res) => {
-    const db = await makeDb({ dbName: "mm_api_demo", collectionName: "contacts" });
     const allData = await db.getAllContacts();
     res.status(200).send(allData);
 }
--- server.js
+++ server.js
@@ -1,39 +1,10 @@
 const express = require("express");
 const app = express();
-const mongodb = require("mongodb");
 const contactsRoutes = require("./routes/contacts.js");
 
 app.use(express.json());
 
-const MongoClient = mongodb.MongoClient;
-const url = 'mongodb://localhost:27017'
-const dbName = 'mm_api_demo'
-const client = new MongoClient(url);
-
 app.use("/contacts", contactsRoutes);
-
-// app.post("/contacts", async (req, res) => {
-//     const { name, email } = req.body;
-
-//     await client.connect();
-//     const db = client.db(dbName);
-//     const collection = db.collection('contacts');
-//     const insertRes = await collection.insertOne({ name, email });
-//     console.log(insertRes);
-
-//     return res.send(insertRes);
-// });
-
-// app.get("/contacts", async (req, res) => {
-
-//     await client.connect();
-//     const db = client.db(dbName);
-//     const collection = db.collection('contacts');
-
-//     const getAllResp = await collection.find({}).toArray();
-
-//     res.send(getAllResp);
-// });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
